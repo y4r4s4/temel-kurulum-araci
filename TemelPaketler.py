58c3654e16d3fb0f3dccb2b48fbaed437e628632
y4r4s4 <103616050+y4r4s4@users.noreply.github.com>
2022-04-14 07:07:23 +0400

    Add files via upload

---
+++ TemelPaketler.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+import os
+
+os.system("clear")
+os.system("termux-setup-storage")
+os.system("apt update")
+os.system("apt upgrade")
+os.system("pkg install git")
+os.system("pkg install curl")
+os.system("pkg install wget")
+os.system("pkg install python")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("pkg install php")
+os.system("pkg install perl")
+os.system("pkg install nano")
+os.system("pkg install vim")
+os.system("pkg install cat")
+os.system("pkg install pip")
+os.system("pkg install pip2")
+os.system("pip install wordlist")
+os.system("pkg install nmap")
+os.system("pkg install hydra")
+os.system("pkg install openssl")
+os.system("pkg install nodejs")
+os.system("pkg install cowsay")
+os.system("pkg install figlet")
+os.system("clear")
+os.system("cowsay HAZIRDIR!")
